8b02f804d9fd14158c9d3829bb07594f94e5c1d7
Camila Gallo <camilagallo1700@gmail.com>
2024-06-07 12:45:03 -0300

    detector

---
+++ stacks_analyzer/detectors/UpdatedFunctionsDetector.py
@@ -0,0 +1,31 @@
+from tree_sitter import Node
+
+from ..print_message import pretty_print_warn
+from ..visitor import Visitor, NodeIterator
+
+
+class UpdatedFunctionsDetector(Visitor):
+    MSG = ""
+    functions_updated = ["element-at", "index-of"]
+
+    def __init__(self):
+        super().__init__()
+        self.fun = ""
+
+    def visit_node(self, node: Node, i):
+        if i > 1:
+            return
+        
+        
+        self.fun = str(node.text, "utf-8")
+
+        if self.fun in self.functions_updated:
+            MSG = f"Behavior of '{self.fun}' changed from Clarity1 to Clarity2, now outputs optional value.\nSuggestion: use '{self.fun}?' to make this behavior explicit."
+            pretty_print_warn(
+                self,
+                node.parent,
+                node,
+                MSG,
+                None
+            )
+
