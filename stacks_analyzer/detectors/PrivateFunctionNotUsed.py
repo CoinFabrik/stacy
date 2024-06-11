2aba3e5772af40b9144f597af09d66bdce886e2d
Facundo Lerena <faculerena@gmail.com>
2024-06-11 11:51:19 -0300

    renamed read-only for private

--- stacks_analyzer/detectors/PrivateFunctionNotUsed.py
+++ stacks_analyzer/detectors/PrivateFunctionNotUsed.py
@@ -7,33 +7,33 @@ from ..visitor import Visitor, NodeIterator
 
 class PrivateFunctionNotUsed(Visitor):
     MSG = "This private function is not used."
-    read_only_names: [Node] = []
+    private_fns_names: [Node] = []
 
     def __init__(self):
         super().__init__()
-        self.read_only_names: [Node] = []
+        self.private_fns_names: [Node] = []
 
     def visit_node(self, node: Node, run_number: int):
         if run_number == 1 and node.grammar_name == "private_function":
-            self.read_only_names.append(node)
+            self.private_fns_names.append(node)
             return
 
         # this can be improved with a better grammar (if not, stx-get-balance and other
         # intrinsic functions will throw as "not used" because they are not defined in the file
         if run_number == 2:
             if node.grammar_name == "contract_function_call":
-                for saved in self.read_only_names:
+                for saved in self.private_fns_names:
                     if saved.child(2).child(1).text == node.child(1).text:
-                        self.read_only_names.remove(saved)
+                        self.private_fns_names.remove(saved)
                         break
 
             if node.grammar_name == "fold" or node.grammar_name == "map" or node.grammar_name == "filter":
-                for saved in self.read_only_names:
+                for saved in self.private_fns_names:
                     if saved.child(2).child(1).text == node.parent.parent.child(2).text:
-                        self.read_only_names.remove(saved)
+                        self.private_fns_names.remove(saved)
 
         if run_number == 3:
-            for n in self.read_only_names:
+            for n in self.private_fns_names:
                 pretty_print_warn(
                     self,
                     n,
@@ -41,4 +41,4 @@ class PrivateFunctionNotUsed(Visitor):
                     self.MSG,
                     None
                 )
-            self.read_only_names = []
+            self.private_fns_names = []
