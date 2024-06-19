81efecff6eab62a8ded6e2877c22e5a2e6d6433f
Camila Gallo <camilagallo1700@gmail.com>
2024-06-19 16:12:50 -0300

    try hard

--- stacks_analyzer/detectors/ArgumentsNotUsed.py
+++ stacks_analyzer/detectors/ArgumentsNotUsed.py
@@ -12,48 +12,41 @@ class ArgumentsNotUsed(Visitor):
     def visit_node(self, node: Node, run_number: int):
         arguments = {}
         if run_number == 1 and node.grammar_name in ["private_function", "read_only_function", "public_function"]:
-            descendants = NodeIterator(node.parent)
+            
+            for child in node.children:
 
-            while True:
-                n = descendants.next()
-                if n is None:
-                    break
+                if child.grammar_name == "function_signature":
+                   #aca guardo todos los parametros que recibe la funcion
+                    for grandchild in child.children:
+                        if grandchild.grammar_name == "function_parameter":
+                            arguments[grandchild.child(1).text.decode("utf-8")] = (0, child) #inicializo todos los argumetos en 0
                 
-                print("SOY N", n.text)
-                
-                if n.grammar_name == "function_parameter":
-                    key = n.child(1).text.decode("utf-8") #name       
-                    arguments[key] = (0, n.child(1))
+                #el resto de la funcion esta adentro del let? CUANDO EXISTE UN LET EL CUERPO SIGUE ACA
+                if child.grammar_name == "let_expression":
+                    for grandchild in child.children:
+                        #si no es local binding, ya tengo todo el cuerpo de mi funcion
+                        if grandchild.grammar_name == "local_binding":
+                            arguments[grandchild.child(1).text.decode("utf-8")] = (0, grandchild)
+                        else:
+                            for key in arguments:
+                                update = arguments[key]
+                                count = update[0] + grandchild.text.decode("utf-8").count(key)
+                                arguments[key] = (count, update[1])
 
-                if n.grammar_name == "let_expression":
-                    for c in n.children:
-                        if c.grammar_name == "local_binding":
-                            if c.child(1).grammar_name == "identifier":
-                                key = c.child(1).text.decode("utf-8")
-                                arguments[key] = (0, c)
+                elif child.grammar_name == "basic_native_form":
+                    for grandchild in child.children:
+                        for key in arguments:
+                                update = arguments[key]
+                                count = update[0] + grandchild.text.decode("utf-8").count(key)
+                                arguments[key] = (count, update[1])
 
-
-                if n.grammar_name == "identifier":
-
-                    key = n.text.decode("utf-8")
-                    print("soy la key!", key)
-                    print("soy el diccionario actual", arguments)
-                    print("==========")
-
-                    if key in arguments:
-                        print("soy la misma key y entre a arguments")
-                        print("==========")
-                        updated_tuple = arguments[key]
-                        updated_tuple = (updated_tuple[0] + 1, updated_tuple[1])
-                        arguments[key] = updated_tuple
-
-
-        for k, v in arguments.items():
-            if v[0] == 0:
-                pretty_print_warn(
-                    self,
-                    v[1],
-                    v[1],
-                    f"'{k}' argument is not used." ,
-                    None
-                )
+            print("arguments", arguments)
+            for k, v in arguments.items():
+                if v[0] == 0:
+                    pretty_print_warn(
+                        self,
+                        v[1],
+                        v[1],
+                        f"'{k}' argument is not used." ,
+                        None
+                    )
